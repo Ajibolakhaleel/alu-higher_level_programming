be41b64dc090e7d82c4116f69908306a2d225c6d
Ajibolakhaleel <113374237+Ajibolakhaleel@users.noreply.github.com>
2022-11-04 10:07:26 +0100

    Update 0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
-quare_matrix_simple(matrix=[]):
-        new_matrix = matrix.copy()
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
 
-            for i in range(len(matrix)):
-                        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-                            return new_matrix
+    return new_matrix
