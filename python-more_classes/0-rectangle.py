6bbfacf7568c1bb2e7faba26ca70e5b898aebe8c
Ajibolakhaleel <113374237+Ajibolakhaleel@users.noreply.github.com>
2022-11-11 15:42:30 +0100

    Update 0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,18 +1,12 @@
- rectangle class"""
+able File  10 lines (7 sloc)  149 Bytes
+
+#!/usr/bin/python3
+"""Define a rectangle class"""
 
 
- class Rectangle:
-     """
-         An empty rectangle class that defines a rectangle
-             """
+class Rectangle:
+    """
+    An empty rectangle class that defines a rectangle
+    """
 
-                 pass#!/usr/bin/python3
-                 """Define a rectangle class"""
-
-
-                 class Rectangle:
-                         """
-                             An empty rectangle class that defines a rectangle
-                                 """
-
-                                     pass
+    pass
