afc9288616ff8c2122f29cb06ac24c71153510cb
Ajibolakhaleel <k.ajibola@alustudent.com>
2022-11-17 09:51:40 -0800

    5

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+
+n that reads a text file (UTF8),
+# and prints it to stdout
+"""
+    define a function 'read_file'
+    """
+
+
+    def read_file(filename=""):
+            """
+                    reads a text file & prints it to stdout
+                        """
+
+                            with open(filename, encoding='utf-8') as f:
+                                        for line in f:
+                                                        print(line, end="")
