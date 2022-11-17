da1dd8b0d55a1038f6aa804d268e06b715673aaf
Ajibolakhaleel <113374237+Ajibolakhaleel@users.noreply.github.com>
2022-11-17 16:12:26 +0100

    Update 9-rectangle.py

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -40,4 +40,4 @@ class Rectangle(BaseGeometry):
 
         string = "[" + str(self.__class__.__name__) + "] "
         string += str(self.__width) + "/" + str(self.__height)
-        return 
+        return string
