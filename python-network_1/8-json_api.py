1043b5fd0323af1fa7cea0a468f3f60d27038514
Ajibolakhaleel <113374237+Ajibolakhaleel@users.noreply.github.com>
2022-11-22 23:17:17 +0100

    Update 8-json_api.py

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""A script tha:
-- takes in a letter
-- sends POST request to http://0.0.0.0:5000/search_user
-with the letter as a parameter.
+# python script that takes in a letter and sends a POST request to,
+# 'http://0.0.0.0:5000/search_user' with the letter as a parameter
+"""
+    take in a letter & send a POST request,
+    to 'http://0.0.0.0:5000/search_user' with the letter as a parameter
 """
 import sys
 import requests
