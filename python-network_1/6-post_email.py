f814b972c441eebafcd8b38496d0b0bbe9b6dd2d
Ajibolakhaleel <113374237+Ajibolakhaleel@users.noreply.github.com>
2022-11-22 23:13:32 +0100

    Update 6-post_email.py

--- python-network_1/6-post_email.py
+++ python-network_1/6-post_email.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""A  script that:
-- takes in a URL and an email address
-- sends a POST request to the passed URL with the email as a parameter
-- displays the body of the response.
+# python script that takes in a URL and an email address,
+# sends a POST request to the passed URL with the email as a parameter,
+# and finally displays the body of the response
+"""
+    send a POST request to the passed URL with the email as a parameter,
+    & displaysthe body of the response
 """
 import sys
 import requests
