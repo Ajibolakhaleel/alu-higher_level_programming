2fdee36e4fcf4ba958441e63d9a7224009ebd052
Ajibolakhaleel <113374237+Ajibolakhaleel@users.noreply.github.com>
2022-11-22 23:04:22 +0100

    Update 2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""A script that:
-- takes in a URL
-- sends a POST request to the passed URL
-- takes email as a parameter
-- displays the body of the response
+# python script that takes in a URL and an email,
+# sends a POST request to the passed URL with the email as a parameter,
+# and displays the body of the response (decoded in utf-8)
+"""
+    send a POST request to the passed URL with the email as a parameter,
+    & display body of the response (decoded in utf-8)
 """
 import sys
 import urllib.parse
