da2a25065e5ecce03871b1d142f59ebb69393a6e
Ajibolakhaleel <113374237+Ajibolakhaleel@users.noreply.github.com>
2022-11-22 23:01:15 +0100

    Update 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""A script that:
-- takes your GitHub credentials (username and password)
-- uses the GitHub API to display your id
+# python script that takes your GitHub credentials (username and password),
+# and uses the 'GitHub API' to display your 'id'
+"""
+    use 'GitHub API to display 'id'
 """
 import sys
 import requests
